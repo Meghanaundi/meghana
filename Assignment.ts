fa5936ee340a545d93c8e3a481688da87456bb52
Meghanaundi <97959286+Meghanaundi@users.noreply.github.com>
2022-07-08 20:03:28 +0530

    Add files via upload

---
+++ Assignment.ts
@@ -0,0 +1,18 @@
+let fibonacci = [0,1];
+   function listFibonacci(num) {
+        for (let i = 1; i < num; i++) {
+            fibonacci.push(fibonacci[i] + fibonacci[i - 1]);
+        }
+        console.log(fibonacci);
+    }
+  listFibonacci(10);
+    function addNumbers(a, b) {  
+        return a + b;  
+    }  
+    var sum = addNumbers("", 25);  
+    console.log('Sum of the numbers is: ' + sum);
+    function isPrime(num) {
+        for(var i = 2; i < num; i++)
+          if(num % i === 0) return false;
+        return num > 1;
+      }
