512122b94a6c042613263db75512ca7b0e2d930d
Meghanaundi <97959286+Meghanaundi@users.noreply.github.com>
2022-07-08 20:05:03 +0530

    Add files via upload

---
+++ ASSIGNMENT3.ts
@@ -0,0 +1,23 @@
+var names = ['Meghana', 'Lekhana'];
+names.push('seetha');
+var array = {
+    name: 'Meghana',
+    age: 19
+};
+array.name = 'meg';
+/* explcit types*/
+var name1;
+var age;
+var isLoggedIn;
+age = 20;
+isLoggedIn = true;
+/union types/
+var mixed = [];
+mixed.push('name');
+mixed.push(45);
+var uid;
+uid = '12345';
+uid = 12;
+var ageone = 25;
+ageone = true;
+console.log(age);
