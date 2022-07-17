db40f836c6e3694244077205f5bd3fa13c1753c5
Meghanaundi <97959286+Meghanaundi@users.noreply.github.com>
2022-07-17 10:14:05 +0530

    Add files via upload

---
+++ functions.ts
@@ -0,0 +1,14 @@
+function abc(){
+    console.log("this is function");
+}
+abc();
+let anony=function abc(){
+    console.log("this is anonymous function");
+}
+anony();
+let s=(a:number,b:number): => {
+  console.log("arrow function");
+    console.log(a*b);
+}
+ s(4,10);
+  
